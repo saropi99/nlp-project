ae29c2bfb8d30f07621d593d93605dcaf9d7a6e9
mivhvl <mehow.kovalski@gmail.com>
2025-03-03 04:10:03 +0000

    Added text preprocessing function (lowercase, remove digits, stopwords, stemming), class distr. function

--- our_preprocessing.py
+++ our_preprocessing.py
@@ -1,18 +1,46 @@
-from nltk import word_tokenize
 from enum import Enum
+import pandas as pd
+import re
 
-from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
 
+nltk.download('stopwords')
 Stemmer = Enum('Stemmer', 'Porter WordNet')
 
-def tokenize_text(text, stemmer=Stemmer.Porter):
-    if stemmer == Stemmer.WordNet:
-        s = WordNetLemmatizer()
-    else:
-        s = PorterStemmer()
+
+def text_preprocess(text, remove_stopwords=True, stemmer=Stemmer.Porter):
+
+    '''
+    Steps:
+        1. using lowercase
+        2. deleting digits
+        3. deleting double spaces
+        4. removing stopwords (optional: True/False)
+        5. deleting puntcuations
+        6. stemming/lemmatization deploying function stem_text() -> returns result
+    '''
+
+    text = text.lower() # convert to lowercase
+    text = re.sub(r'\d+', '', text)  # delete all digits
+    text = re.sub(r'\s+', ' ', text)  # delete double spaces
+
+    # stemming with stopwords removal
+    if remove_stopwords:
+        stop_words = set(stopwords.words('english'))
+        text = ' '.join([word for word in word_tokenize(text) if word not in stop_words])
+
+    text = re.sub(r'[^\w\s]', '', text)  # remove punctuations but after removing stopwords bc of the stopwords list
+
+    return stem_text(text, stemmer)
+
+def stem_text(text, stemmer=Stemmer.Porter):
+
     words = word_tokenize(text)
     clean = []
+    s = WordNetLemmatizer() if stemmer == Stemmer.WordNet else PorterStemmer()
     for word in words:
         if stemmer == Stemmer.WordNet:
             stem = s.lemmatize(word)
@@ -20,3 +48,15 @@ def tokenize_text(text, stemmer=Stemmer.Porter):
             stem = s.stem(word)
         clean.append(stem)
     return clean
+
+def class_distribution(dataset):
+    if 'sentiment_label' in dataset.columns:
+        counts = dataset.sentiment_label.value_counts()
+    elif 'sarcasm_label' in dataset.columns:
+        counts = dataset.sarcasm_label.value_counts()
+    else:
+        print('No relevant labels found in DataFrame.')
+        return
+    percentages = counts / counts.sum() * 100
+    distribution_df = pd.DataFrame({'Count': counts, 'Percentage': percentages.round(2)})
+    print(distribution_df)

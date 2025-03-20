595e70b616154205b1ee6190bb2a2af0a28934e0
pedro.pereira <pedro.pereira@jscrambler.com>
2025-03-20 16:20:32 +0000

    Add Recursive Feature Selection

--- our_feature_selection.py
+++ our_feature_selection.py
@@ -1,4 +1,7 @@
-from sklearn.feature_selection import SelectKBest, f_classif, chi2, mutual_info_classif, SelectPercentile
+from sklearn.feature_selection import SelectKBest, f_classif, chi2, mutual_info_classif, SelectPercentile, RFECV
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 def feat_filtering(X_train, y_train, X_test, method='percentile', k=10, func='chi2'):
     if func == 'f':
@@ -15,3 +18,25 @@ def feat_filtering(X_train, y_train, X_test, method='percentile', k=10, func='ch
     X_train_redux = sel.fit_transform(X_train, y_train)
     X_test_redux = sel.transform(X_test)
     return sel, X_train_redux, X_test_redux
+
+def rfe(X_train, y_train, X_test, step=0.1, min_features_to_select=1, cv=5, model='svm'):
+    if model == 'logistic':
+        model = LogisticRegression()
+    elif model == 'rf':
+        model = RandomForestClassifier()
+    else:
+        model = SVC(kernel='linear')
+
+    sel = RFECV(
+            estimator=model,
+            step=step,
+            cv=cv,
+            scoring="accuracy",
+            min_features_to_select=min_features_to_select,
+            n_jobs=2,
+        )
+    X_train_redux = sel.fit_transform(X_train, y_train)
+    X_test_redux = sel.transform(X_test)
+    return sel, X_train_redux, X_test_redux
+
+    

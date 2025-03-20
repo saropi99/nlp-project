ddb7867c620fbcb21ad39c5dfdc38a89675241e6
Michal Kowalski <me@Michals-MacBook-Air.local>
2025-03-20 03:41:16 +0000

    text_preprocess() update using spacy for lemmatization -> outperforms nltk

--- our_preprocessing.py
+++ our_preprocessing.py
@@ -1,92 +1,73 @@
-from enum import Enum
-import pandas as pd
 import re
-import textblob
+import spacy
 import contractions
-
+from textblob import TextBlob
 import nltk
-from nltk import word_tokenize, pos_tag
-from nltk.corpus import stopwords, wordnet
-from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from enum import Enum
 
-
-
-nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('averaged_perceptron_tagger_eng')
 
+stop_words = set(stopwords.words('english'))
+important_words = {"not", "no", "nor", "cannot", "again"}
+stop_words -= important_words
+
+nlp = spacy.load('en_core_web_sm')
 
 Stemmer = Enum('Stemmer', 'Porter WordNet')
 
-def text_preprocess(text, remove_stopwords=True, remove_digits=True, stemmer=Stemmer.Porter):
+# Text Preprocessing Func.
+def text_preprocess(text, remove_digits=True, stemmer=Stemmer.Porter):
 
     '''
     Steps:
-        1. using lowercases
-        2. removing urls, emails from text
-        3. removing digits (optional bc of the POS tagging)
-        4. correcting misspellings
-        5. splitting contractions: can't into cannot, i'm into i am
-        6. stemming / pos + lemmatization using func. stem_text()
-        7. removing stopwords (optional: True/False)
-        8. deleting puntcuations
-        9. deleting double spaces, possible spaces at the beg. and end
+        1. use just lowercases
+        2. remove urls, emails from text
+        3. remove digits (optional bc of the POS tagging)
+        4. correct misspellings
+        5. split contractions: can't into cannot, i'm into i am
+        6. lemmatize with pos or stem, and remove stopwords except negations
+        7. remove stopwords except negations (optional: True/False)
+        8. delete punctuations
+        9. delete double spaces, possible spaces at the beg. and end
     '''
 
     text = text.lower() # just lowercase
-    text = re.sub(r'\S*https?:\S*', '', text) # remove urls
-    text = re.sub(r'\S+@\S+', '', text) # remove emails
+    text = re.sub(r'\S*https?:\S*', '', text) # remove possible urls
+    text = re.sub(r'\S+@\S+', '', text) # remove possible emails
 
     if remove_digits:
-        text = re.sub(r'\d+', '', text)  # remove digits
+        text = re.sub(r'\d+', '', text) # remove digits
 
-    corrected_text = textblob.TextBlob(text).correct() # correct misspellings
-    text = contractions.fix(str(corrected_text)) # split contractions
-
-    text = stem_text(text, stemmer) # stemmer/lemmatizer
-
-    # stopwords removal
-    if remove_stopwords:
-        important_words = {"not", "no", "nor", "cannot"}
-        stop_words = set(stopwords.words('english'))
-        stop_words -= important_words
-        text = ' '.join([word for word in word_tokenize(text) if word not in stop_words])
-
-    text = re.sub(r'[^\w\s]', '', text) # delete punctuations (after stopwords removing just in case)
-    text = re.sub(r'\s+', ' ', text).strip()   # delete double spaces, at the beginning and end
-
-    return text
-
-def stem_text(text, stemmer=Stemmer.Porter):
-
-    words = word_tokenize(text)
-    clean = []
+    corrected_text = TextBlob(text).correct() # correct any misspellings
+    text = str(corrected_text)
+    text = contractions.fix(text) # split contractions
 
     if stemmer == Stemmer.WordNet:
-        s = WordNetLemmatizer()
-        pos_tags = pos_tag(words)
-        for word, tag in pos_tags:
-            stem = s.lemmatize(word, get_wordnet_pos(tag))
-            clean.append(stem)
-    else:
-        s = PorterStemmer()
-        for word in words:
+        doc = nlp(text) # lemmatization + stopwords removing
+        stemm_text = ' '.join([token.lemma_ for token in doc if token.text not in stop_words])
+
+    if stemmer == Stemmer.Porter: # stemming + stopwords removing
+        stemm_text = stem_text(text)
+
+    stemm_text = re.sub(r'[^\w\s]', '', stemm_text) # delete punctuations (after stopwords removing just in case)
+    stemm_text = re.sub(r'\s+', ' ', stemm_text).strip() # delete possible double spaces, at the beginning and end
+
+    return stemm_text
+
+# Stem Function
+def stem_text(text):
+    words = word_tokenize(text)
+    clean = []
+    s = PorterStemmer()
+    for word in words:
+        if word not in stop_words:
             stem = s.stem(word)
             clean.append(stem)
-
     return ' '.join(clean)
-
-def get_wordnet_pos(tag):
-    if tag.startswith('J'):
-        return wordnet.ADJ  # Adjective
-    elif tag.startswith('V'):
-        return wordnet.VERB  # Verb
-    elif tag.startswith('N'):
-        return wordnet.NOUN  # Noun
-    elif tag.startswith('R'):
-        return wordnet.ADV  # Adverb
-    else:
-        return wordnet.NOUN

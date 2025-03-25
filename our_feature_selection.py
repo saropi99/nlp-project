28a601394e46bd80156fa114b68136f24b2b29e3
pedro.pereira <pedro.pereira@jscrambler.com>
2025-03-25 13:27:14 +0000

    Run pipreqs, refactor notebook and add saving rfe selector

--- our_feature_selection.py
+++ our_feature_selection.py
@@ -2,6 +2,7 @@ from sklearn.feature_selection import SelectKBest, f_classif, chi2, mutual_info_
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+import pickle
 
 def feat_select_model(model='svm'):
     if model == 'logistic':
@@ -29,10 +30,10 @@ def feat_filtering(X_train, y_train, X_test, method='percentile', k=10, func='ch
     X_test_redux = sel.transform(X_test)
     return sel, X_train_redux, X_test_redux
 
-def rfe(X_train, y_train, X_test, step=0.1, min_features_to_select=1, cv=5, model='svm'):
-    model = feat_select_model(model)
+def rfe(X_train, y_train, X_test, step=0.1, min_features_to_select=1, cv=5, model='svm', save_file=False):
+    _model = feat_select_model(model)
     sel = RFECV(
-            estimator=model,
+            estimator=_model,
             step=step,
             cv=cv,
             scoring="accuracy",
@@ -41,4 +42,9 @@ def rfe(X_train, y_train, X_test, step=0.1, min_features_to_select=1, cv=5, mode
         )
     X_train_redux = sel.fit_transform(X_train, y_train)
     X_test_redux = sel.transform(X_test)
+
+    if save_file:
+        with open("rfecv_" + model + ".pickle", "wb") as fh:
+            pickle.dump(sel, fh)
+
     return sel, X_train_redux, X_test_redux
